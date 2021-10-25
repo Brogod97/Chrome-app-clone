cbee57d706f969dfc1fa5532018c51fdecd07e1a
Hyeongshin <evergreen1286@gmail.com>
2021-10-25 17:28:57 +0900

    Update style

--- js/background.js
+++ js/background.js
@@ -1,17 +1,15 @@
 const images = [
   "chrome-wallpaper1.jpg",
-  "chrome-wallpaper2.jpg",
-  "chrome-wallpaper3.jpg",
-  "chrome-wallpaper4.jpg",
   "chrome-wallpaper5.jpg",
   "chrome-wallpaper6.jpg",
   "chrome-wallpaper7.jpg",
-  "chrome-wallpaper8.jpg",
 ];
 
+const background = "background";
 const choesenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.className = background;
 
 bgImage.src = `images/${choesenImage}`;
 
